41d4f707056d4bc55ccdee077bc240c6466874d6
Ahmad Alikaj <yewe1847@in.uni-weimar.de>
2019-11-01 17:18:17 +0100

    finished implementation

--- Circle.java
+++ Circle.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 
 public class Circle {
-	//todo: add some methods to compute a circle's diameter, circumference and area.
-	//print some outputs to confirm your methods are working correctly.
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double radius = scan.nextDouble();
+        //Calculate Diameter
+        double diameter = calcDiameter(radius);
+        System.out.println("Diameter is " + diameter);
+        System.out.printf( "Diameter is %.2f\n", diameter );
+        //Calculate Circumferemce
+        double circumference = calcCircumference(radius);
+        System.out.println("Circumference is " + circumference);
+        System.out.printf( "Circumference is %.2f\n", circumference );
+        //Calculate Area
+        double area = calcArea(radius);
+        System.out.println("Area is " + area);
+        System.out.printf( "Area is %.2f\n", area );
+
+
+    }
+
+    public static double calcDiameter (double radius) {
+        return radius *2;
+}
+
+    public static double calcCircumference  (double radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    public static double calcArea  (double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
 }
--- Prime.java
+++ Prime.java
@@ -1,7 +1,34 @@
-import java.util.Scanner;
-
-
 public class Prime {
+    public static void main(String[] args) {
+        int number = 11;
+        int number2 = 9;
+        boolean result = isPrime(number);
+        System.out.println(result);
+        int j = findDoublette(number2);
+        System.out.println("Doubblet are " + j + " and " + (j+2));
+    }
+
+    //Method checking if number is prime
+    public static boolean isPrime (int number) {
+        boolean status = false;
+        for (int i = 3; i<number; i++ ){
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Method finds the first doublette
+    public static int findDoublette (int number2) {
+        for (int j= number2; j < Integer.MAX_VALUE ; j++ ) {
+            if (isPrime(j) && isPrime(j+2)) {
+                return j;
+            }
+        }
+
+        return -1;
+    }
 		
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
